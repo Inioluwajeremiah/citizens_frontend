34bae0fcdb17e9b852a23c0235fb20410fe1f60a
adewarainioluwa@gmail.com <adewara@gmail.com>
2024-11-11 13:05:48 +0100

    base url updated

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -3,10 +3,11 @@
 //   policyUrl: "policy/",
 // };
 
+// "proxy": " https://citizens-backend-lzzl.onrender.com",
 // "proxy": "http://localhost:7000",
 export const endpoints = {
   // baseUrl: "http://localhost:7000/api/v1/",
-  baseUrl: "https://citizens-backend-lzzl.onrender.com",
+  baseUrl: "https://citizens-backend-lzzl.onrender.com/api/v1/",
   policyUrl: "policy/",
   blogUrl: "blog/",
 } as const;
