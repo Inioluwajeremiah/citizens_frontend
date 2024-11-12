c132f81a6b4c9297dfee4f020ce1cd6169cce153
adewarainioluwa@gmail.com <adewara@gmail.com>
2024-11-12 10:15:40 +0100

    public assets

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,5 +1,37 @@
-const Blog = () => {
-  return <div>Blog</div>;
+const AboutUs = () => {
+  // const purpose =
+  //   "Youth Track it is an accountability platform established by the Democracy Convening Alliance. It serves as an online scorecard, amplifying the voices of young people and marginalized communities in Nigeria. This tool tracks progress on the demands outlined in the Youth Democracy Charter of Demand and provides vital data to guide advocacy efforts and influence policy formulation, implementation, and reform where needed.";
+
+  return (
+    <>
+      <section
+        className="w-full mt-20 h-[467px] relative  bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url('/src/assets/images/herobg.png')`,
+        }}
+      >
+        {/* <div className="absolute h-[467px]  top-0 inset-0 bg-gradient-to-b from-[rgba(11,29,43,0)] to-[rgba(11,29,43,1)] z-10"> */}
+        <div className="absolute h-[467px] top-0 inset-0 bg-gradient-to-b from-[rgba(220,238,236,0.4)] to-[#062422] z-10">
+          <div className=" w-[80%] lg:w-full container mx-auto h-full  flex flex-col justify-end  ">
+            <div className="flex flex-col md:flex-row items-center justify-center md:justify-between mb-10">
+              <p className="text-[40px] text-center md:text-left text-white leading-[70px] w-[80%] md:w-[40%] lg:w-[25%] font-medium tracking-[-1.6px]">
+                YouthTrackIt
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-[80%] lg:w-full container mx-auto flex flex-col items-center justify-center py-20 bg-white ">
+        <p className="text-2xl lg:text-[40px] font-semibold text-center">
+          Blogs <span className=" text-blackColor tracking-[-1.6px]"></span>
+        </p>
+        <p className="mt-10 text-base lg:text-2xl lg:leading-[40px] text-center">
+          No blog posts have been published yet.
+        </p>
+      </section>
+    </>
+  );
 };
 
-export default Blog;
+export default AboutUs;
